939be374c2dd61b3ef946baf048888ff8fb9e2d2
Trevor Wilson <trevorbwilson89@gmail.com>
2019-06-22 19:37:57 -0500

    Updates

---
+++ precache-manifest.587ca98df76fa205975c94012afe7697.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445430832f6752db220c0021dcc2970b",
+    "url": "/reactmemorygame/index.html"
+  },
+  {
+    "revision": "4748b0f062558d83b55a",
+    "url": "/reactmemorygame/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/reactmemorygame/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "4748b0f062558d83b55a",
+    "url": "/reactmemorygame/static/js/main.8dca5288.chunk.js"
+  },
+  {
+    "revision": "834b4ac589a3a1aeb945",
+    "url": "/reactmemorygame/static/js/runtime~main.e9c274db.js"
+  }
+]);
